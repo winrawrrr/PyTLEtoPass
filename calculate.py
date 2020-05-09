c1aac018ed3d4946c03759805d33783479976ac7
winrawrrr <winrawrrr@programmer.net>
2020-05-08 22:06:57 -0400

    Added Comments to main script

--- calculate.py
+++ calculate.py
@@ -15,24 +15,32 @@ LONG = 21.942
 passhour = 0
 
 while passhour < 24:
+    # set the date with a variable for the hour
     d = datetime(2020, 5, 9, passhour, 0, 0)
     orb = Orbital('CUBESAT', 'cubesat.txt')
+    # generate pass information for the hour specified
     passes = orb.get_next_passes(d, 1, LONG, LAT, ALTITUDE_ASL, horizon=1)
-
+    # if there is no pass for the current value of passhour, the length will be zero
     if len(passes) > 0:
         rise_az, rise_el = orb.get_observer_look(passes[0][0], LONG, LAT, ALTITUDE_ASL)
         transit_az, transit_el = orb.get_observer_look(passes[0][2], LONG, LAT, ALTITUDE_ASL)
         set_az, set_el = orb.get_observer_look(passes[0][1], LONG, LAT, ALTITUDE_ASL)
+
+        # print out the pass info
         print("CUBESAT")
         print(passes[0][0], rise_az, rise_el)
         print(passes[0][2], transit_az, transit_el)
         print(passes[0][1], set_az, set_el)
         print()
-
+        # use datetime.strptime() to split the pass time into its componenet parts
+        # set the hour of the current pass
         passtring = datetime.strptime(str(passes[0][0]), '%Y-%m-%d %H:%M:%S.%f')
         passhour = passtring.hour
+        # iterate to the next hour
         passhour += 1
+
     else:
-        passhour +=1
+        # iterate to the next hour
+        passhour += 1
 
 
